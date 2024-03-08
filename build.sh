9f02bfe803cb1cdcc3e906995477d8b13b11fd63
flamendless <flamendless8@gmail.com>
2024-03-08 21:08:21 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,23 @@
 ks=""
 
+sign_apk () {
+    sudo apksigner sign --ks "$ks" app/build/outputs/apk/embedNoRecord/release/app-embed-noRecord-release-unsigned.apk
+}
+
+sign_aab () {
+    sudo apksigner sign --min-sdk-version 19 --ks "$ks" app/build/outputs/bundle/embedNoRecordRelease/app-embed-noRecord-release.aab
+}
+
 apk () {
-    sudo ./gradlew assembleEmbedNoRecordRelease && sudo apksigner sign --ks "$ks" app/build/outputs/apk/embedNoRecord/release/app-embed-noRecord-release-unsigned.apk
+    sudo ./gradlew assembleEmbedNoRecordRelease && sign_apk
 }
 
 aab () {
-    sudo ./gradlew bundleEmbedNoRecordRelease && sudo apksigner sign --min-sdk-version 19 --ks "$ks" app/build/outputs/bundle/embedNoRecordRelease/app-embed-noRecord-release.aab
+    sudo ./gradlew bundleEmbedNoRecordRelease && sign_aab
 }
+
+if [ "$#" -eq 0 ]; then
+    echo "wrong usage"
+else
+	"$1" "$@"
+fi
